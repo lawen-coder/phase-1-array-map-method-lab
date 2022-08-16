8b7e766129e8fe3560a67f2255a5a846f8239063
lawen <lawensardar16@gmail.com>
2022-08-16 21:18:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -12,5 +12,24 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  
+  return tutorials.map(str => {
+   
+ 
+     
+   
+    str = str.split(' ');
+    for (let i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+    return str.join(' ');
+    
+       
+    
+      
+   
+  })
+
+  
+  
 }
